0ba38310666820ea3b4fc438a40d5c66078ff820
Evgeniy Kukarskiy <jon.79555@gmail.com>
2023-09-08 13:22:47 +0700

    added comments

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ with conn.cursor() as cur:
         """)
     conn.commit()
 
+# функция создания БД
+
 def bd_creator(conn):
     with conn.cursor() as cur:
         cur.execute("""
@@ -22,6 +24,8 @@ def bd_creator(conn):
             """)
         conn.commit()
 
+# Функция добавления клиента
+
 def add_client(conn, table: str, first_name: str, last_name: str,
                mail: str, phone: list = []):
     with conn.cursor() as cur:
@@ -33,6 +37,9 @@ def add_client(conn, table: str, first_name: str, last_name: str,
         id_client = cur.fetchone()[0]
     print(f'Клиент {last_name} {first_name} добавлен успешно. Его id: {id_client}.')
 
+# Функция получения телефонов клиента. Дополнительная, нужна для реализации логики
+# работы с данными в этом атрибуте
+
 def get_phone(conn, table:str, id_client: int)-> list:
     with conn.cursor() as cur:
         cur.execute(f"""
@@ -41,6 +48,10 @@ def get_phone(conn, table:str, id_client: int)-> list:
             """, (id_client,))
         phone = cur.fetchone()
     return phone
+
+# Функция добавления телефона существующему клиенту.
+# Логика в том, чтобы получить текущий список по клиенту,
+# добавить в этот список новый, удалить старые данные и записать обновленные
 
 def add_phone(conn, table: str, phone: str, id_client: int):
     phone_old = get_phone(conn, table, id_client)
@@ -58,6 +69,8 @@ def add_phone(conn, table: str, phone: str, id_client: int):
         conn.commit()
     print(f'Телефон {phone} добавлен.')
 
+# Функция изменения данных клиента
+
 def data_change(conn, table: str, id_client: int, atr: str, data: str):
     if atr == 'phone':
         add_phone(conn, table, data, id_client)
@@ -71,6 +84,8 @@ def data_change(conn, table: str, id_client: int, atr: str, data: str):
     else:
         print(f'Такого атрибута нет.')
 
+# Удаление телефона клиента
+
 def del_phone(conn, table: str, id_clients: int):
     phone = []
     with conn.cursor() as cur:
@@ -80,6 +95,8 @@ def del_phone(conn, table: str, id_clients: int):
         conn.commit()
         print(f'Данные удалены.')
 
+# Функция удаления клиента
+
 def del_client(conn, table: str, id_client: int):
     with conn.cursor() as cur:
         cur.execute(f"""
@@ -88,24 +105,48 @@ def del_client(conn, table: str, id_client: int):
         conn.commit()
         print(f'Данные о клиенте id: {id_client} удалены')
 
+# Функция поиска клиента
+
 def find_client(conn, table: str, atr: str, value: str) -> int:
     with conn.cursor() as cur:
         cur.execute(f"""
-            SELECT id FROM {table}
+            SELECT id, first_name, last_name, mail, phone FROM {table}
             WHERE {atr}=%s;
         """, (value,))
         print(cur.fetchone())
 
+# создание базы
+
 bd_creator(conn)
+
+# Добавление клиентов
+
 add_client(conn, 'clients', 'Евгений', 'Кукарский', 'jon.795@mail.ru')
+add_client(conn, 'clients', 'Томас', 'Кукарский', 'jon.795555@mail.ru')
+
+# Добавление телефона
+
 add_phone(conn, 'clients', '1234567890', 1)
+
+# изменение данных по клиенту
+
 data_change(conn, 'clients', '1', 'first_name', 'Женя')
 data_change(conn, 'clients', '1', 'phone', '3216549870')
+
+# Удаление телефона клиента
+
 del_phone(conn, 'clients', 1)
+
+# Добавление телефона
+
 add_phone(conn, 'clients', '1234567890', 1)
-find_client(conn, 'clients', 'first_name', 'Евгений')
-phones = get_phone(conn, 'clients', 1)
+
+# Поиск клиента
+
+find_client(conn, 'clients', 'first_name', 'Томас')
+
+# Удаление клиента
 del_client(conn, 'clients', 1)
-print(phones)
+
 
 conn.close()
